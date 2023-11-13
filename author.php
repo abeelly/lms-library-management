1cccec32caf355ec728ffddcaab546ad2fddf43e
elly <80318068+abeelly@users.noreply.github.com>
2023-11-13 23:04:15 +0800

    Add files via upload

---
+++ author.php
@@ -0,0 +1,8 @@
+<?php
+include('connect.php');
+session_start();
+include 'header.php';
+include('navbar.php');?>
+    <!-- login area start -->
+    <!-- login area end -->
+    <?php include('footer.php');?>
